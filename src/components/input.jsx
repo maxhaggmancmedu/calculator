05d59d03faff13a09d682b38490ce97c196ade56
Max Häggman <max.haggman@edu.cmeducations.se>
2023-04-27 11:55:41 +0200

    clean up file

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,6 +1,6 @@
 import '../styles/input.css'
 
-export default function Input({updateNumbers, inputValue, evaluateInput, handleKeyDown}) {
+export default function Input({updateNumbers, inputValue, handleKeyDown}) {
 
     return (
         <input type='text' placeholder='0' value={inputValue} onChange={updateNumbers} onKeyDown={handleKeyDown} autoFocus onBlur={({ target }) => target.focus()}></input>
